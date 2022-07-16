144c57ae953ff2a9efa5b0544cbab3c160685c15
Mohammad Sakib Chowdhury <c193055@ugrad.iiuc.ac.bd>
2022-07-16 23:44:36 +0600

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-parseInt(prompt("Enter a number from 1 to 5 :"));
+//Guessing game
+
+var numWin = 0;
+var numLose = 0;
+
+for (i = 0; i < 5; i++) {
+  var guessNum = parseInt(prompt("Enter a number from 1 to 5 :"));
+  var randomNum = Math.floor(Math.random() * 5) + 1;
+
+  if (guessNum == randomNum) {
+    console.log("You have won");
+    numWin++;
+  } else {
+    console.log("You have lost. Random number was " + randomNum);
+    numLose++;
+  }
+}
+document.write("Time's UP<br>");
+document.write("Won = " + numWin + "</br>");
+document.write("Lost = " + numLose + "</br>");
